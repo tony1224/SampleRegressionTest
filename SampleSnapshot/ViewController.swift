a501d0ee2db23c1eb762111b206adaca84dc4399
jun-morita <jun.morita@gree.net>
2020-09-05 18:20:47 +0900

    Initial Commit

---
+++ SampleSnapshot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleSnapshot
+//
+//  Created by Jun Morita on 2020/09/05.
+//  Copyright © 2020 Jun Morita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
